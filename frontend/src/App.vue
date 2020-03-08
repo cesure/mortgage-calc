5dadc74ad36c3dabac917a1d64d9a058efda49c5
Christian Schulze <christian.schulze@cesure.de>
2020-03-08 19:59:34 +0100

    Add Typescript

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,30 @@
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
 </template>
 
-<script>
-    import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+  import HelloWorld from './components/HelloWorld.vue';
 
-    export default {
-        name: 'App',
-        components: {
-            HelloWorld
-        }
-    }
+  @Component({
+    components: {
+      HelloWorld,
+    },
+  })
+  export default class App extends Vue {
+  }
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
