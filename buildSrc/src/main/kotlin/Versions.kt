45a7cdcd0572eb9a2bd824769b41666c302f5181
Christian Schulze <christian.schulze@cesure.de>
2020-04-17 20:34:34 +0200

    Update ktor to 1.3.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,7 +9,7 @@ object Versions {
     object Dependency {
         val jackson = "2.9.9"
         val kotlin = Plugin.kotlin
-        val ktor = "1.3.0"
+        val ktor = "1.3.2"
         val logback = "1.2.1"
         val node = "12.16.1"
     }
