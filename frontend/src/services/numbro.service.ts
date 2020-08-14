bd034e1f4125a1bf43972cc3007b55d781fd913b
Christian Schulze <christian.schulze@cesure.de>
2020-08-14 12:47:09 +0200

    numbro: Add type definition for German language file

--- frontend/src/services/numbro.service.ts
+++ frontend/src/services/numbro.service.ts
@@ -1,55 +1,7 @@
 import numbro from "numbro";
+import languageDeDe from "numbro/dist/languages/de-DE.min.js";
 
-const languageDe: numbro.NumbroLanguage = {
-  languageTag: "de-DE",
-  delimiters: {
-    thousands: ".",
-    decimal: ",",
-    thousandsSize: 3
-  },
-  abbreviations: {
-    thousand: "k",
-    million: "m",
-    billion: "b",
-    trillion: "t"
-  },
-  ordinal: function () {
-    return ".";
-  },
-  spaceSeparated: true,
-  currency: {
-    symbol: "€",
-    position: "postfix",
-    code: "EUR"
-  },
-  currencyFormat: {
-    totalLength: 4,
-    thousandSeparated: true
-  },
-  formats: {
-    fourDigits: {
-      totalLength: 4,
-      spaceSeparated: true,
-      average: true
-    },
-    fullWithTwoDecimals: {
-      output: "currency",
-      mantissa: 2,
-      spaceSeparated: true,
-      thousandSeparated: true
-    },
-    fullWithTwoDecimalsNoCurrency: {
-      mantissa: 2,
-      thousandSeparated: true
-    },
-    fullWithNoDecimals: {
-      output: "currency",
-      spaceSeparated: true,
-      thousandSeparated: true,
-      mantissa: 0
-    }
-  }
-};
+numbro.registerLanguage(languageDeDe, true);
 
 class NumbroService {
 
@@ -65,10 +17,6 @@ class NumbroService {
     trimMantissa: true,
     thousandSeparated: true
   };
-
-  constructor() {
-    numbro.registerLanguage(languageDe, true);
-  }
 
   formatCurrency(value: number): string {
     return numbro(value).formatCurrency(this.format);
---
+++ frontend/src/types/numbro.d.ts
@@ -0,0 +1,6 @@
+declare module "numbro/dist/languages/de-DE.min.js" {
+  import numbro from "numbro";
+
+  const value: numbro.NumbroLanguage;
+  export default value;
+}
