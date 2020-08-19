bfacd2871445454f50062d55c9cab998db8de0c4
Christian Schulze <christian.schulze@cesure.de>
2020-08-19 08:28:16 +0200

    Update Kotlin and ktor to version 1.4.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
 
     object Plugin {
-        val kotlin = "1.3.72"
+        val kotlin = "1.4.0"
         val ktlint = "9.2.1"
         val nodeGradle = "2.2.3"
         val shadow = "5.2.0"
@@ -10,7 +10,7 @@ object Versions {
     object Dependency {
         val jackson = "2.9.9"
         val kotlin = Plugin.kotlin
-        val ktor = "1.3.2"
+        val ktor = "1.4.0"
         val logback = "1.2.1"
         val node = "12.18.3"
     }
