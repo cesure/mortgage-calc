a211c0fd8ef2883f504d3ce5b3b4a095916647fc
Christian Schulze <christian.schulze@cesure.de>
2021-01-05 21:25:21 +0100

    fritz2 WIP

--- src/jsMain/kotlin/com.github.cesure.mortgagecalc/components/InputComponents.kt
+++ src/jsMain/kotlin/com.github.cesure.mortgagecalc/components/InputComponents.kt
@@ -27,5 +27,13 @@ fun RenderContext.numberInput(baseClass: String? = null, id: String? = null, val
 fun RenderContext.percentageInput(baseClass: String? = null, id: String? = null, value: Flow<String>?): Input {
     return input(id = id) {
         value?.let { value(it) }
+
+//        focuss handledBy SimpleHandler<Unit>()
+//        changes.values() handledBy
     }
 }
+
+//val testLens: Lens<String, String> = format(
+//    { "+$it" },
+//    { "$it+" }
+//)
