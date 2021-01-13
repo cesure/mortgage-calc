bae87986fa4a3ca9269ee36157d740a9d45154e7
Christian Schulze <christian.schulze@cesure.de>
2021-01-13 21:51:38 +0100

    WIP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.targets.js.NpmVersions
 
 plugins {
-    kotlin("multiplatform") version "1.4.21"
-    kotlin("plugin.serialization") version "1.4.21"
-    id("dev.fritz2.fritz2-gradle") version "0.8"
+    kotlin("multiplatform") version Versions.Plugin.kotlin
+    kotlin("plugin.serialization") version Versions.Plugin.kotlin
+    id("dev.fritz2.fritz2-gradle") version Versions.Plugin.fritz2
 }
 
 repositories {
@@ -15,7 +15,7 @@ kotlin {
     jvm {
     }
 
-    js {
+    js(IR) {
         browser {
             binaries.executable()
         }
@@ -24,8 +24,8 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.1")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:${Versions.Dependency.kotlinxDatetime}")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.Dependency.kotlinxSerializationJson}")
             }
         }
         val jvmMain by getting {
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,13 @@
+object Versions {
+
+    object Plugin {
+        val fritz2 = "0.8"
+        val kotlin = "1.4.21"
+    }
+
+    object Dependency {
+        val kotlin = Plugin.kotlin
+        val kotlinxDatetime = "0.1.1"
+        val kotlinxSerializationJson = "1.0.1"
+    }
+}
