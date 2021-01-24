a8a3f23acc6971f3685f98ad8ed5775770c79f71
Christian Schulze <christian.schulze@cesure.de>
2021-01-24 14:43:41 +0100

    WIP

--- src/jvmMain/kotlin/com/github/cesure/mortgagecalc/model/RepaymentPlanCalc.kt
+++ src/jvmMain/kotlin/com/github/cesure/mortgagecalc/model/RepaymentPlanCalc.kt
@@ -3,11 +3,11 @@ package com.github.cesure.mortgagecalc.model
 import kotlinx.datetime.LocalDate
 
 fun Mortgage.calculateRepaymentPlan(): RepaymentPlan {
-    val dummyPayment = Payment(
+    val firstPayment = Payment(
         type = PaymentType.PAYOUT,
         date = LocalDate(2020, 1, 1),
         amount = this.totalAmount.times(-1),
         balanceAfter = this.totalAmount.times(-1),
     )
-    return repaymentPlan(dummyPayment)
+    return repaymentPlan(firstPayment)
 }
